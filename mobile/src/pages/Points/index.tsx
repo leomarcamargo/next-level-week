c61bec1cf394ae4d9f94ab76665adfee9cac39b9
Leomar Camargo <leomarcamargodesouza@gmail.com>
2020-06-05 22:06:24 -0300

    Fifth class

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -26,6 +26,7 @@ interface Item {
 interface Point {
   id: number;
   image: string;
+  image_url: string;
   name: string;
   latitude: number;
   longitude: number;
@@ -146,7 +147,7 @@ const Points = () => {
                     <Image
                       style={styles.mapMarkerImage}
                       source={{
-                        uri: point.image,
+                        uri: point.image_url,
                       }}
                     ></Image>
                     <Text style={styles.mapMarkerTitle}>{point.name}</Text>
--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -11,7 +11,7 @@ class ItemController {
                 title: item.title,
                 image_url: `http://192.168.0.16:3333/uploads/${item.image}`
             };
-        })
+        });
     
         return response.json(serializedItems);
     }
